8bb27a7b74d929e47217ff525c479b76032f8d04
goneone <37247010+goneone@users.noreply.github.com>
2018-08-02 22:38:29 +0900

    tt

--- src/example/arrayListExample.java
+++ src/example/arrayListExample.java
@@ -8,10 +8,14 @@ public class arrayListExample {
  public static void main(String[] args) {
 	
 	 String data = "07,08,09,10,5,";
+	 String data2 = " ";
+	 String data3 = "a";
 	 String[] items = data.split(",");
 	 ArrayList<String> spMonthList = new ArrayList<String>(Arrays.asList(items));
 	 System.out.println(spMonthList);
 	 System.out.println(spMonthList.size());
 	 //System.out.println(spMonthList.get(0));
+	 
+	 System.out.println(data3.length());
 }
 }
