9c0389d6a134617f89998757127b519be320c970
k24u2001 <k24u2001@naver.com>
2019-01-02 16:15:37 +0900

    uncompletePlayer2 - hashMap 사용

--- src/programmersAlgorithm/uncompletePlayer.java
+++ src/programmersAlgorithm/uncompletePlayer.java
@@ -12,26 +12,19 @@ public class uncompletePlayer {
 		for (int i = 0; i < completion.length; i++) {
 			if (!completion[i].equals(participant[i])) {
 				answer = participant[i];
+				break; //이 break 하나로 답이 갈림.
 			}
 		}
 		if (answer.equals("")) {
 			answer = participant[completion.length];
-		} else {
-			answer = answer;
 		}
 		return answer;
 	}
 
 	public static void main(String[] args) {
-		String[] testA = new String[4];
-		String[] testB = new String[3];
-		testA[0] = "lock1";
-		testA[1] = "lock2";
-		testA[2] = "lock3";
-		testA[3] = "lock4";
-		testB[0] = "lock1";
-		testB[1] = "lock2";
-		testB[2] = "lock3";
+		String[] testA = { "leo", "kiki", "eden" };
+		String[] testB = { "eden", "kiki" };
+
 		System.out.println(uncompletePlayer.solution(testA, testB));
 
 	}
---
+++ src/programmersAlgorithm/uncompletePlayer2.java
@@ -0,0 +1,40 @@
+package programmersAlgorithm;
+
+import java.util.HashMap;
+
+import java.util.Iterator;
+
+public class uncompletePlayer2 {
+	// using hashmap
+	public static String solution(String[] participant, String[] completion) {
+		String answer = "";
+		HashMap<String, Integer> hash = new HashMap<>();
+		// 이름을 키값, 인원수를 밸류로 넣는다.
+		// getOrDefault 메소드는 키에 해당하는 밸류가 없으면 두번째 인자, 있으면 첫번쨰 인자를 리턴함.
+		// player는 배열의 배열요소임.
+		for (String player : participant) {
+			hash.put(player, hash.getOrDefault(player, 0) + 1);
+		}
+		// completion 만큼 돌면서 밸류값 빼줌.
+		for (String player2 : completion) {
+			hash.put(player2, hash.get(player2) - 1);
+		}
+
+		// keySet은 키값 다 리턴함.
+		for (String key : hash.keySet()) {
+
+			if (hash.get(key) != 0) {
+				answer = key;
+			}
+		}
+		System.out.println(hash.keySet());
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		String[] testA = { "leo", "kiki", "eden" };
+		String[] testB = { "eden", "kiki" };
+
+		System.out.println(uncompletePlayer2.solution(testA, testB));
+	}
+}
